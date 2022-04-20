a295286dcdeae3d6c927689d4f28a8e7864f8ba9
Antonio Seraus <aseraus@hotmail.com>
2022-04-20 19:47:38 +0200

    Setters and Getters

--- src/Computer.java
+++ src/Computer.java
@@ -4,12 +4,43 @@ public class Computer {
     String Monsterboard;
     String processor;
 
+    public void setCPU(String CPU) {
+        this.CPU = CPU;
+    }
+
+    public void setHarddisk(String harddisk) {
+        this.harddisk = harddisk;
+    }
+
+    public void setMonsterboard(String monsterboard) {
+        this.Monsterboard = monsterboard;
+    }
+
+    public void setProcessor(String processor) {
+        this.processor = processor;
+    }
+
+    public String getHarddisk() {
+        return harddisk;
+    }
+
+    public String getMonsterboard() {
+        return Monsterboard;
+    }
+
+    public String getProcessor() {
+        return processor;
+    }
+
+
+
+    public String getCPU() {
+        return CPU;
+    }
+
     public void addInfo(){
 
-        this.CPU = "Amidon";
-        this.harddisk="2.9TByte";
-        this.Monsterboard = "Celeron";
-        this.processor = "Raid";
+
     }
 
 }
--- src/Objects.java
+++ src/Objects.java
@@ -3,10 +3,13 @@ public class Objects {
     public static void main(String[] args) {
         Computer cp1 = new Computer();
 
-        cp1.addInfo();
-        System.out.println(cp1.CPU);
-        System.out.println(cp1.harddisk);
-        System.out.println(cp1.Monsterboard);
+        cp1.setCPU("AMIDON");
+        cp1.setMonsterboard("Hardcore");
+        cp1.setHarddisk("2.9 Terrabyte");
+
+        System.out.println(cp1.getCPU());
+        System.out.println(cp1.getHarddisk());
+        System.out.println(cp1.getMonsterboard());
     }
 
 }
