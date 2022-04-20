34ab4438cac36b5733bc0a2ce3ddefae6134a885
Antonio Seraus <aseraus@hotmail.com>
2022-04-20 17:22:23 +0200

    arrays added

---
+++ src/Operators.java
@@ -0,0 +1,15 @@
+public class Operators {
+
+     public static void main(String[] args) {
+         int [] myArray = new int[3];
+         myArray[0] = 100;
+         myArray[1] = 200;
+         myArray[2] = 300;
+
+         int [] myArray1 = {1,2,3};
+
+         System.out.println(myArray[0]);
+         System.out.println(myArray[1]);
+         System.out.println(myArray[2]);
+    }
+}
