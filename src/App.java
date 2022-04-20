5d8c74ad12436d6b893046615a7557de227f8518
Antonio Seraus <aseraus@hotmail.com>
2022-04-20 14:51:22 +0200

    variables created

--- src/App.java
+++ src/App.java
@@ -1,6 +1,9 @@
 public class App {
 
     public static void main(String[] args) {
+        String str = "String123";
+        int integerNumber= 123;
+        double doubleNumber = 2.45;
         System.out.println("Hello world");
     }
 }
