a0f4919b6c8dae3fc352c9b73d9819a1fc709030
Antonio Seraus <aseraus@hotmail.com>
2022-04-20 17:05:54 +0200

    primitive Data Reference

--- src/App.java
+++ src/App.java
@@ -3,13 +3,21 @@ public class App {
     public static void main(String[] args) {
         String str = "String123";
         int integerNumber= 123;
+        Integer integerNumberRef = 123;
         double doubleNumber = 2.45;
         short shortNumber= 32767;
+        Short shortNumberref = 32767;
         long longNumber = 37139231;
+        Long longNumberRef = Long.valueOf(37139231);
         float floatNumber = 52.3f;
+        Float floatNumerRef = 52.3f;
         char character = 'a';
+        Character characterRef = 'a';
         boolean booleanValue = true;
+        Boolean booleanValueRef = false;
+        System.out.println(Boolean.getBoolean(String.valueOf(booleanValue)));
         byte myByte = 120;
+        Byte myByteRef = 120;
         System.out.println("Hello world");
     }
 }
