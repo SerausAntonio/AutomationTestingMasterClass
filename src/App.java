18ba5349c8a8536b41cfcfb2fcb9439e83a3589c
Antonio Seraus <aseraus@hotmail.com>
2022-04-20 15:26:44 +0200

    primitive data types added

--- src/App.java
+++ src/App.java
@@ -4,6 +4,12 @@ public class App {
         String str = "String123";
         int integerNumber= 123;
         double doubleNumber = 2.45;
+        short shortNumber= 32767;
+        long longNumber = 37139231;
+        float floatNumber = 52.3f;
+        char character = 'a';
+        boolean booleanValue = true;
+        byte myByte = 120;
         System.out.println("Hello world");
     }
 }
